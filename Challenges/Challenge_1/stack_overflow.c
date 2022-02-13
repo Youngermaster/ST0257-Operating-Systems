f56a73b0bcad319a056d6d90ef3a20315604d00d
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2022-02-13 11:44:16 -0500

    Stack values added

--- Challenges/Challenge_1/stack_overflow.c
+++ Challenges/Challenge_1/stack_overflow.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+int counter(int counter) {
+    int anotherCounter = 17;
+    printf("Counter function %p %p %p\n", &counter, &anotherCounter, __builtin_frame_address(0));
+}
 
 int main(int argc, char const *argv[]) {
     printf("This is a way to crash the program with a stack overflow\n");
+    int i = 13;
+    printf("Main function %p %p\n", &i, __builtin_frame_address(0));
+    counter(18);
     return 0;
 }
